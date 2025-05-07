587d659061f985ec32baffc16bb4a7e477af5969
HAN ZAW HTET <hanzawhtet317@gmail.com>
2025-05-08 00:50:00 +0630

    feat: add footer and activity card(shadcn) section components

---
+++ src/components/core/layout/Footer.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Footer() {
+	return (
+		<footer className="border-t">
+			<p>
+				Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+				Officiis, eos?
+			</p>
+		</footer>
+	);
+}
--- src/components/core/layout/Layout.tsx
+++ src/components/core/layout/Layout.tsx
@@ -1,12 +1,14 @@
 import { Outlet } from "react-router";
+import Footer from "./Footer";
 
 const Layout = () => {
-  return (
-    <div>
-      <h3>Layout</h3>
-      <div>{<Outlet />}</div>
-    </div>
-  );
+	return (
+		<div className="min-h-screen">
+			<h3>Layout</h3>
+			<div>{<Outlet />}</div>
+			<Footer />
+		</div>
+	);
 };
 
 export default Layout;
