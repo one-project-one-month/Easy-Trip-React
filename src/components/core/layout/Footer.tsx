a122edaa4a2c444446961c9147b6f236d4624535
HAN ZAW HTET <hanzawhtet317@gmail.com>
2025-05-09 17:04:18 +0630

    feat: add trip plan route and create TripPlan  page

--- src/components/core/layout/Footer.tsx
+++ src/components/core/layout/Footer.tsx
@@ -1,12 +1,19 @@
-import React from "react";
-
 export default function Footer() {
 	return (
-		<footer className="border-t">
-			<p>
-				Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-				Officiis, eos?
-			</p>
+		<footer className="border-t bg-gray-900 text-white">
+			<div className="container mx-auto flex justify-between py-5">
+				<p>
+					This project is created under a one month one project hosted
+					by Sann Lynn Htun
+				</p>
+
+				<div>
+					<ul className="flex gap-5">
+						<li>About us</li>
+						<li>Contact</li>
+					</ul>
+				</div>
+			</div>
 		</footer>
 	);
 }
--- src/components/core/layout/Layout.tsx
+++ src/components/core/layout/Layout.tsx
@@ -3,9 +3,9 @@ import Footer from "./Footer";
 
 const Layout = () => {
 	return (
-		<div className="min-h-screen">
+		<div className="flex flex-col min-h-screen">
 			<h3>Layout</h3>
-			<div>{<Outlet />}</div>
+			<div className="grow">{<Outlet />}</div>
 			<Footer />
 		</div>
 	);
