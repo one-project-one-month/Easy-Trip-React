6b694f74e687283b6200023eb4889c993089bb8a
HAN ZAW HTET <hanzawhtet317@gmail.com>
2025-05-26 22:46:09 +0630

    feat(footer): footer added

--- src/components/core/layout/Footer.tsx
+++ src/components/core/layout/Footer.tsx
@@ -3,37 +3,98 @@ import {
   TwitterLogoIcon,
   LinkedInLogoIcon,
   GitHubLogoIcon,
-  MagicWandIcon,
 } from "@radix-ui/react-icons";
+import { Icons } from "@/shared/icons/Icons";
 import { Link } from "react-router";
 
 export default function Footer() {
   return (
     <footer className="border-t ">
-      <div className="container mx-auto max-sm:flex-col gap-3 max-sm:text-center flex items-center justify-between py-3 px-5 md:px-10">
-        <div className="">
-          <h1 className="sm:text-xl text-lg flex items-center gap-2 mb-2">
-            <MagicWandIcon />
-            <span>Easy Trip</span>
-          </h1>
-          <ul className="flex sm:gap-5 gap-1 flex-col sm:flex-row max-sm:text-xs">
-            <li>
-              <Link to="/">Terms & Conditions</Link>
-            </li>
-            <li>
-              <Link to="/">Privacy Policy</Link>
-            </li>
-          </ul>
+      <div className="container mx-auto px-5 md:px-10 py-5 flex flex-col sm:flex-row justify-between gap-5">
+        <div className="flex items-start">
+          <div className="flex items-center">
+            <Icons.logo />
+            <h1 className="text-2xl font-bold">Easy Trip</h1>
+          </div>
         </div>
 
-        <div className="flex flex-col gap-2">
-          <div className="flex gap-2 max-sm:justify-center justify-end">
-            <InstagramLogoIcon className="sm:w-[25px] sm:h-[25px]" />
-            <GitHubLogoIcon className="sm:w-[25px] sm:h-[25px]" />
-            <TwitterLogoIcon className="sm:w-[25px] sm:h-[25px]" />
-            <LinkedInLogoIcon className="sm:w-[25px] sm:h-[25px]" />
+        <div className="grid grid-cols-2 md:grid-cols-3 gap-5 lg:gap-20">
+          <div className="flex flex-col gap-1">
+            <h1 className="text-lg font-medium">Contact Us</h1>
+            <ul className="space-y-1">
+              <li>
+                <Link to="#" className="hover:underline hover:text-blue-600">
+                  Customer Support
+                </Link>
+              </li>
+              <li>
+                <Link to="#" className="hover:underline hover:text-blue-600">
+                  Service Guarantee
+                </Link>
+              </li>
+              <li>
+                <Link to="#" className="hover:underline hover:text-blue-600">
+                  More Service Info
+                </Link>
+              </li>
+            </ul>
           </div>
-          <p className="text-xs md:text-base md:text-end">
+          <div className="flex flex-col gap-1">
+            <h1 className="text-lg font-medium">About</h1>
+            <ul className="space-y-1">
+              <li>
+                <Link to="#" className="hover:underline hover:text-blue-600">
+                  About Easy Trip
+                </Link>
+              </li>
+              <li>
+                <Link to="#" className="hover:underline hover:text-blue-600">
+                  News
+                </Link>
+              </li>
+              <li>
+                <Link to="#" className="hover:underline hover:text-blue-600">
+                  Terms & Conditions
+                </Link>
+              </li>
+              <li>
+                <Link to="#" className="hover:underline hover:text-blue-600">
+                  Privacy Statement
+                </Link>
+              </li>
+            </ul>
+          </div>
+          <div className="flex flex-col gap-1">
+            <h1 className="text-lg font-medium">Contact Us</h1>
+            <ul className="space-y-1">
+              <li>
+                <Link to="#" className="hover:underline hover:text-blue-600">
+                  Customer Support
+                </Link>
+              </li>
+              <li>
+                <Link to="#" className="hover:underline hover:text-blue-600">
+                  Service Guarantee
+                </Link>
+              </li>
+              <li>
+                <Link to="#" className="hover:underline hover:text-blue-600">
+                  More Service Info
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        <div className="flex flex-col gap-4 sm:items-center md:items-end">
+          <div className="flex gap-4">
+            <InstagramLogoIcon className="md:w-6 md:h-6 w-4 h-4 hover:scale-110 transition-transform duration-300 cursor-pointer" />
+            <GitHubLogoIcon className="md:w-6 md:h-6 w-4 h-4 hover:scale-110 transition-transform duration-300 cursor-pointer" />
+            <TwitterLogoIcon className="md:w-6 md:h-6 w-4 h-4 hover:scale-110 transition-transform duration-300 cursor-pointer" />
+            <LinkedInLogoIcon className="md:w-6 md:h-6 w-4 h-4 hover:scale-110 transition-transform duration-300 cursor-pointer" />
+          </div>
+
+          <p className="text-xs md:text-end">
             Easy Trip 2025 © All Rights Reserved.
           </p>
         </div>
