f3b811e59be4c1f6aef92452ec76333e9d79d618
nyominkhat <nyominkhat329@gmail.com>
2025-06-07 23:18:09 +0630

    feat: added map

--- src/components/core/layout/PublicLayout.tsx
+++ src/components/core/layout/PublicLayout.tsx
@@ -1,8 +1,15 @@
-import { Outlet } from "react-router";
+import { Outlet, useLocation } from "react-router";
 
 import Navbar from "./Navbar";
+import { useEffect } from "react";
 
 const PublicLayout = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo({ top: 0, behavior: "instant" });
+  }, [pathname]);
+
   return (
     <main className="flex flex-col min-h-screen">
       <Navbar />
